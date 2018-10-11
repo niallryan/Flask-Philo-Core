56fc6f716c9ce08bbdeb1f81743d875d336432b8
Rondineli <raraujo04@yahoo.com.br>
2018-10-11 12:12:04 +0100

    Tests running

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,32 +1,24 @@
 import argparse
 import subprocess
+import os
 
 
 def main():
     description = 'Run a command in docker'
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument(
-        '--container_id', help='Run to docker ps to get the container id ',
-        required=True)
 
     parser.add_argument('--test', required=False, default='app')
 
     args, extra_params = parser.parse_known_args()
 
-    test_cmd = 'pytest -s -q /philo/tests/{}'.format(
+    test_cmd = "pytest -s -q /philo/tests/{}".format(
         args.test)
 
     # test_cmd = 'pip3 install  webcolors'
 
-    cmd = [
-        'docker',
-        'exec',
-        '-it',
-        args.container_id,
-        'sh',
-        '-c',
-        test_cmd
-    ]
+    cmd_entrypoint = "docker-compose run --rm --volume={}/../:/philo python sh -c".format(os.getcwd())
+    cmd = cmd_entrypoint.split(" ")
+    cmd.append(test_cmd)
 
     try:
         subprocess.call(cmd)
